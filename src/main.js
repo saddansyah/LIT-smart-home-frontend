f1292f2feb9f9ee79f6d2335dff245e064083a5f
saddansyah <saddanakbar@gmail.com>
2023-02-15 23:00:58 +0700

    add vuetify while using vite

--- src/main.js
+++ src/main.js
@@ -4,10 +4,22 @@ import App from "./App.vue";
 import router from "./router";
 import store from './stores'
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
 import "./assets/main.css";
 
+// Init
 const app = createApp(App);
+const vuetify = createVuetify({
+    components,
+    directives,
+  })
 
 app.use(store);
 app.use(router);
+app.use(vuetify);
 app.mount("#app");

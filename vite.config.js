3d1e3cededc8350139655766f38d89fc5e3aee6c
saddansyah <saddanakbar@gmail.com>
2023-02-15 20:33:25 +0700

    init vuetify and headless ui

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,16 @@ import vue from "@vitejs/plugin-vue";
 
 import svgLoader from "vite-svg-loader";
 
+// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
+import vuetify from 'vite-plugin-vuetify'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), svgLoader()],
+  plugins: [
+		vue(),
+		svgLoader(),
+		vuetify({ autoImport: true }),
+	],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
